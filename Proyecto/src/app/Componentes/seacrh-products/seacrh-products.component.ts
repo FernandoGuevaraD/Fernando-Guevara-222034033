68169cb167c634f544bfa23a308fd821e1e9f911
FernandoGuevaraD <fernandoguevara@udenar.edu.co>
2024-12-04 09:20:45 -0500

    comit 1

---
+++ Proyecto/src/app/Componentes/seacrh-products/seacrh-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seacrh-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './seacrh-products.component.html',
+  styleUrl: './seacrh-products.component.css'
+})
+export class SeacrhProductsComponent {
+
+}
