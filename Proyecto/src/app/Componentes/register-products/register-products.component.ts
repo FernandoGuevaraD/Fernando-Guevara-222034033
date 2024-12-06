9509b640b9a52bc73a0fa4754d2b187d3490cf4a
FernandoGuevaraD <fernandoguevara@udenar.edu.co>
2024-12-05 23:32:07 -0500

    comit 2

--- Proyecto/src/app/Componentes/register-products/register-products.component.ts
+++ Proyecto/src/app/Componentes/register-products/register-products.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { PasswordModule } from 'primeng/password';
 
 @Component({
   selector: 'app-register-products',
   standalone: true,
-  imports: [],
+  imports: [InputTextModule, ButtonModule, FormsModule, ReactiveFormsModule, PasswordModule,CommonModule],
   templateUrl: './register-products.component.html',
   styleUrl: './register-products.component.css'
 })
-export class RegisterProductsComponent {
+export class RegisterProductsComponent implements OnInit{
+  register_productform: FormGroup = null!;
+  productName: string = '';
+  purchasePrice: number | null = null;
+  quantity: number | null = null;
+  salePrice: number | null = null;
 
+  constructor(private fb: FormBuilder){
+
+  }
+
+  ngOnInit()  { 
+    this.register_productform = this.fb.group({
+      productName: ['', Validators.required],
+      purchasePrice: ['', Validators.required],
+      quantity: ['', Validators.required],
+      salePrice : ['',Validators.required]
+    });
+  }
+
+
+    
+    /* estas variable solo estan para que corra el servidor aqui ya toa meterles los servicios*/
+  registerProduct() {
+    console.log('Producto registrado:', {
+      nombre: this.productName,
+      precioCompra: this.purchasePrice,
+      cantidad: this.quantity,
+      precioVenta: this.salePrice,
+    });
+
+    if (!this.productName || !this.purchasePrice || !this.quantity || !this.salePrice) {
+      alert('Todos los campos son obligatorios.');
+      return;
+    }
+
+    alert('Producto registrado exitosamente.');
+  }
 }
--- Proyecto/src/app/app.routes.ts
+++ Proyecto/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Componentes/login/login.component';
+import { RegisterComponent } from './Componentes/register/register.component';
+import { RegisterProductsComponent } from './Componentes/register-products/register-products.component';
+import { SeacrhProductsComponent } from './Componentes/seacrh-products/seacrh-products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ 
+    {
+        path: '', redirectTo: 'home', pathMatch: 'full'
+    },
+    {
+        path: 'login', 
+        component: LoginComponent
+    },
+    {
+        path: 'register', 
+        component: RegisterComponent
+    },
+    {
+        path: 'register_products', 
+        component: RegisterProductsComponent
+    },
+    {
+        path: 'search', 
+        component: SeacrhProductsComponent
+    }
+
+];
